feb725bd80ee0cc1c72694d786e9dee6bcb0b691
tanisha <tanishaudawat34@gmail.com>
2024-06-09 13:18:16 +0530

    basic

---
+++ my.js
@@ -0,0 +1,19 @@
+/*const profile={
+    name : "tanisha udawat",
+   post:5,
+   followers:500,
+   headline:"mern-stack intern",
+   skills:"html-css",
+}
+console.log(profile);*/
+
+//arithmetic operators 
+/*let a =5;
+let b =2;
+console.log("a=",a,"&b=",b);
+console.log("a+b=",a+b);*/
+
+//unary operator
+let a = 5;
+let b =2;
+console.log("a=",a,"&b=",b);
