22bbebf6d3d4b422c84a77397525da844de55fcd
tanisha@123 <110999040+tanisha112@users.noreply.github.com>
2024-06-10 15:26:09 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,26 @@
+a=10
+b=20
+c=6
+d=4
+//console.log(a+b)
+//console.log(c-b)
+//console.log(a*b)
+//console.log(a/b)
+
+function add (a,b){
+    console.log(a+b)
+}
+function minus (a,b){
+    console.log(a-b)
+}
+function multiply (a,b){
+    console.log(a*b)
+}
+function divide (a,b){
+    console.log(a/b)
+}
+(function(){
+    console.log("hello")
+})
+
+//what is different between parameter and arguments
